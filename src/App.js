182b5960bc9e93d9eaf46e949f669f4bcdfbfe47
Lưu Xuân Trường <xuantruong1234@gmail.com>
2021-01-24 11:01:44 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -11,20 +11,29 @@ const SimpleButton = SimpleStyledComponent.Button`
   font-weight: ${(props) => props.theme?.fontWeight}
 `;
 
+const SimpleButton1 = SimpleStyledComponent.Button`
+  font-weight: bold;
+  color: ${(props) => props.theme?.textColor};
+  font-size: ${(props) => props.theme?.fontSize};
+  font-weight: ${(props) => props.theme?.fontWeight}
+`;
+
 const SimpleInput = SimpleStyledComponent.Input`
 `;
 
 export default function App() {
   return (
     <SimpleStyledProvider
-      theme={{ textColor: "blue", fontSize: "30px", fontWeight: "bold" }}
+      theme={{ textColor: "blue", fontSize: "20px", fontWeight: "bold" }}
     >
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
-        <SimpleButton theme={{ fontSize: "30px" }} onClick={() => alert(1)}>
-          Hello fff
-        </SimpleButton>
+        <SimpleButton onClick={() => alert(1)}>Button 1</SimpleButton>
+        <br />
+        <SimpleButton1 theme={{ fontSize: "35px" }} onClick={() => alert(1)}>
+          Button 2
+        </SimpleButton1>
         <br />
         <SimpleInput />
       </div>
